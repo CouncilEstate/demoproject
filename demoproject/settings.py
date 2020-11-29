24e0dd4022c730f7975712177b00fe14f57a7041
Jed Hussein <jed@Jeds-MacBook-Air.local>
2020-11-29 16:38:47 +0000

    pchange

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'employeedb',
         'USER': 'postgres',
-        'PASSWORD': '10citiesofganja',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
